fa7de69b1fa41ba2ee7bdf1b098cecad9b237fe7
Vijay Raghuwanshi <ervijayraghuwanshi@gmail.com>
2026-01-13 23:58:26 +0530

    Github Actions Workflow

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/spark-sql-builder/', // REPLACE with your actual repository name
   plugins: [
     react(),
     tailwindcss(),
