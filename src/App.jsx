c2b3e9a840441bde798ad2deccee58a164a9da57
Vijay Raghuwanshi <ervijayraghuwanshi@gmail.com>
2026-01-14 09:15:27 +0530

    build config fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,18 @@ import Home from "./components/Home"
 import App_v1 from "./components/App_v1";
 import App_v2 from "./components/App_v2";
 import App_v3 from "./components/App_v3";
-import { BrowserRouter, Routes, Route, HashRouter} from "react-router-dom";
-
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 function App() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/1" element={<App_v1 />} />
                 <Route path="/2" element={<App_v2 />} />
                 <Route path="/3" element={<App_v3 />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
