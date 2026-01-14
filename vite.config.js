6e4efd02dbe7c4a62083180880be7b4c35ac4a9b
Vijay Raghuwanshi <ervijayraghuwanshi@gmail.com>
2026-01-14 09:20:28 +0530

    router fix

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/spark-sql-builder/', // REPLACE with your actual repository name
+  base: '/spark-sql-builder/',
   plugins: [
     react(),
     tailwindcss(),
